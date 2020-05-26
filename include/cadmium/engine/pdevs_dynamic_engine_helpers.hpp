704e6b482361a42ba28eed2bce10bd5e2a1e6542
Guillermo G. Trabes <guillermotrabes@sce.carleton.ca>
2020-05-26 12:37:46 -0400

    small chage on concurrent version backtracked

--- include/cadmium/engine/pdevs_dynamic_engine_helpers.hpp
+++ include/cadmium/engine/pdevs_dynamic_engine_helpers.hpp
@@ -97,7 +97,7 @@ namespace cadmium {
             #ifdef CADMIUM_EXECUTE_CONCURRENT
             template<typename TIME>
             void init_subcoordinators(TIME t, subcoordinators_type<TIME>& subcoordinators, boost::basic_thread_pool* threadpool) {
-                auto init_coordinator = [&t, threadpool, thread_number](auto & c)->void { c->init(t, threadpool); };
+                auto init_coordinator = [&t, threadpool](auto & c)->void { c->init(t, threadpool); };
                 std::for_each(subcoordinators.begin(), subcoordinators.end(), init_coordinator);
             }
             #else
