969c1f5389bf1f7cde5a507f2be201f9331ef136
Guillermo G. Trabes <guillermotrabes@sce.carleton.ca>
2020-05-26 12:29:43 -0400

    backtracking changes on concurrent version

--- include/cadmium/engine/pdevs_dynamic_engine_helpers.hpp
+++ include/cadmium/engine/pdevs_dynamic_engine_helpers.hpp
@@ -96,8 +96,8 @@ namespace cadmium {
 
             #ifdef CADMIUM_EXECUTE_CONCURRENT
             template<typename TIME>
-            void init_subcoordinators(TIME t, subcoordinators_type<TIME>& subcoordinators, boost::basic_thread_pool* threadpool, int thread_number) {
-                auto init_coordinator = [&t, threadpool, thread_number](auto & c)->void { c->init(t, threadpool, thread_number); };
+            void init_subcoordinators(TIME t, subcoordinators_type<TIME>& subcoordinators, boost::basic_thread_pool* threadpool) {
+                auto init_coordinator = [&t, threadpool, thread_number](auto & c)->void { c->init(t, threadpool); };
                 std::for_each(subcoordinators.begin(), subcoordinators.end(), init_coordinator);
             }
             #else
@@ -110,7 +110,7 @@ namespace cadmium {
 
             #ifdef CADMIUM_EXECUTE_CONCURRENT
             template<typename TIME>
-            void advance_simulation_in_subengines(TIME t, subcoordinators_type<TIME>& subcoordinators, boost::basic_thread_pool* threadpool, int thread_number) {
+            void advance_simulation_in_subengines(TIME t, subcoordinators_type<TIME>& subcoordinators, boost::basic_thread_pool* threadpool) {
                 auto advance_time= [&t](auto &c)->void { c->advance_simulation(t); };
 
                 if (threadpool == nullptr) {
